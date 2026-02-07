152107c3a8cfd30feb050256c6f30c94de0ab242
Wal1yS <valera.tinyakov@gmail.com>
2026-02-08 02:21:22 +0300

    404 special handling added

--- backend/src/main/java/com/example/istoki_diary/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/istoki_diary/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 import com.example.istoki_diary.exception.CustomException;
 
 @ControllerAdvice
@@ -17,7 +18,11 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<CustomExceptionDTO> handleException(Exception ex) {
+    public ResponseEntity<CustomExceptionDTO> handleException(Exception ex) throws Exception {
+        if (ex instanceof NoResourceFoundException) {
+            throw ex;
+        }
+        ex.printStackTrace();
         CustomExceptionDTO error = new CustomExceptionDTO("GENERAL_ERROR", "An unexpected error occurred.");
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
